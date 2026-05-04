906c71ff6a94bbc6f6510dd5230b28fc8210468a
leejunho12316 <49345305+leejunho12316@users.noreply.github.com>
2026-05-04 09:45:43 +0900

    [level 2] Title: 타겟 넘버, Time: 472.22 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,4 @@
+# stack dfs
 def solution(numbers, target):
     
     answer = 0
@@ -18,3 +19,24 @@ def solution(numbers, target):
         stack.append((ind + 1, sum - numbers[ind]))
         
     return answer
+
+
+# 재귀 dfs
+# def solution(numbers, target):
+#     global answer
+#     answer = 0
+    
+#     def dfs(i, sum):
+#         global answer
+
+#         if i == len(numbers): #최대 횟수 끝났으면
+#             if sum == target:
+#                 answer += 1
+#             return
+                
+#         dfs(i + 1, sum + numbers[i])
+#         dfs(i + 1, sum - numbers[i])
+    
+#     dfs(0, 0)
+    
+#     return answer
