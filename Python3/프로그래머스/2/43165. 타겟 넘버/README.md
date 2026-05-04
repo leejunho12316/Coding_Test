0088351c4d7c635c888a7bdd7cd96ef8543d15d7
leejunho12316 <49345305+leejunho12316@users.noreply.github.com>
2026-05-04 09:44:46 +0900

    [level 2] Title: 타겟 넘버, Time: 491.25 ms, Memory: 9.39 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/README.md
+++ Python3/프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 349.84 ms
+메모리: 9.39 MB, 시간: 491.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 04일 09:28:33
+2026년 05월 04일 09:44:43
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,20 @@
 def solution(numbers, target):
-    global answer
-    answer = 0
     
-    def dfs(i, sum):
-        global answer
-
-        if i == len(numbers): #최대 횟수 끝났으면
+    answer = 0
+    stack = [(0,0)]
+    
+    while stack:
+        # stack으로부터 하나 꺼내기
+        ind, sum = stack.pop()
+        
+        # 조건 확인
+        if ind == len(numbers):
             if sum == target:
                 answer += 1
-            return
-                
-        dfs(i + 1, sum + numbers[i])
-        dfs(i + 1, sum - numbers[i])
-    
-    dfs(0, 0)
-    
+            continue
+            
+        # 분기 2개 stack에 저장
+        stack.append((ind + 1, sum + numbers[ind]))
+        stack.append((ind + 1, sum - numbers[ind]))
+        
     return answer
